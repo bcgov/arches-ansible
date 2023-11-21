9127b885eabd08847beb977157b5aa38a82402bd
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2023-11-20 20:43:17 -0800

    613 - Updated docker image for endowment provisioning scripts 431 - Update postgres client version to match PG 16

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.8.12
+FROM python:3.9
 LABEL maintainer="QED"
 
 # Install envconsul
@@ -6,7 +6,13 @@ RUN curl -sSL https://releases.hashicorp.com/envconsul/0.11.0/envconsul_0.11.0_l
     && tar -zxvf /tmp/envconsul_0.11.0_linux_amd64.tgz -C /usr/local/bin \
     && rm /tmp/envconsul_0.11.0_linux_amd64.tgz
 
-RUN apt-get update && apt-get install -y sudo-ldap vim postgresql-client
+RUN apt-get update && apt-get install -y sudo-ldap vim lsb-release
+
+# Get and activate the postgres repository
+RUN sh -c 'echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list' \
+    && curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+
+RUN apt-get update && apt-get install -y postgresql-client-16
 
 # Non-root user
 RUN useradd -ms /bin/bash runner
@@ -17,6 +23,8 @@ RUN chown runner:runner /apps_ux
 # The rest is done as runner
 USER runner
 WORKDIR /home/runner
+
+RUN ssh-keygen -t ecdsa -N '' -f ~/.ssh/id_ecdsa
 
 COPY --chown=runner ./files/* ./
 RUN chmod 755 ./*.sh
--- files/run_playbook.sh
+++ files/run_playbook.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export ANSIBLE_COLLECTIONS_PATH=./collections
+#export PLAYBOOK_DIR=.
 export PLAYBOOK_DIR=$ANSIBLE_COLLECTIONS_PATH/ansible_collections/bcgov_arches/arches_deployer/playbooks
 
 if [ -z "$1" ]; then
@@ -15,12 +16,26 @@ if [ ! -f $PLAYBOOK_FILE ]; then
   exit 1
 fi
 
-
 [ ! -f "./local_vars.yml" ] && touch ./local_vars.yml
 
-ansible-playbook \
-   -i collections/ansible_collections/bcgov_arches/arches_deployer/inventory/sandbox \
-   --extra-vars @./local_vars.yml \
-   $PLAYBOOK_FILE
-
-
+if [[ $PLAYBOOK_FILE =~ .*(arches|data|arches-purge-backups)\.yml ]]; then
+   if [ -z "$3" ]; then
+     echo "Must specify playbook to execute. eg ./run_arches_playbook $PLAYBOOK_FILE <dlvr|test> <bcfms|bcrhp>"
+     exit 1
+   else
+      export APP_INSTANCE=$2
+      export APP_ACRONYM=$3
+      ansible-playbook \
+         -i collections/ansible_collections/bcgov_arches/arches_deployer/inventory/sandbox \
+         --vault-password-file=./vault_passwd.txt \
+         --extra-vars @./local_vars.yml -vvv \
+         --extra-vars "app_instance=$2 app_acronym=$3 $4 $5 $6" \
+         $PLAYBOOK_FILE
+   fi
+else
+  ansible-playbook \
+     -i collections/ansible_collections/bcgov_arches/arches_deployer/inventory/sandbox \
+     --vault-password-file=./vault_passwd.txt \
+     --extra-vars @./local_vars.yml -vvv \
+     $PLAYBOOK_FILE
+fi
